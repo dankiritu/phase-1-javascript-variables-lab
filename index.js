536deb8ff5900de5b15978d7b1deb850e9ffa5f6
dankiritu <daniel.kiritu@student.moringaschool.com>
2024-06-28 20:27:52 +0300

    complete variables lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution in this file!
+{
+ let companyName = "Scuber";
+}
+
+{
+ const companyName ="Scuber";
+}
+
+{
+    let mostProfitableNeighborhood;
+    mostProfitableNeighborhood = "Chelsea"
+}
+
+{
+    let companyCeo;
+    companyCeo ="Susan Smith"
+}
